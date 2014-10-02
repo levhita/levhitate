71b51c0b1587d138724a892218d68d94c20a08de
Levhita <levhita@gmail.com>
2014-10-02 18:07:58 -0500

    fixes #1

--- levhitate.js
+++ levhitate.js
@@ -12,33 +12,36 @@ Only works well in block elements, best used to animate levhita's logo on his ho
 
   $.fn.levhitate = function(options) {
     var opts = $.extend({}, $.fn.levhitate.settings, options )
-
-    if(opts.stop==true){
-      //Stop the levitation, reseting to original state and stoping interval
-      clearInterval(this.data('lev_interval'));
-      this.data('lev_stop', true);
-      return this;
-    }
-
-    // Gets and Save original position data so we can restore it later
-    opts.padding_top = parseInt(this.css('padding-top'));
-    opts.padding_bottom = parseInt(this.css('padding-bottom'));
-
-    this.data('lev_padding-top', opts.padding_top);
-    this.data('lev_padding-bottom', opts.padding_bottom);
     
-    // Give the element room to levhitate
-    this.animate({'padding-top': opts.padding_top+opts.range}, opts.speed*.8);
-    
-    
-    var element = this;
-    var interval = setInterval(function() {
-      applyLevitation(element, opts);  
-    }, opts.speed);
+    this.each(function() {
+      
+      if(opts.stop==true){
+        //Stop the levitation, reseting to original state and stoping interval
+        clearInterval($(this).data('lev_interval'));
+        $(this).data('lev_stop', true);
+        return true;
+      }
+      
+      // Gets and Save original position data so we can restore it later
+      opts.padding_top = parseInt($(this).css('padding-top'));
+      opts.padding_bottom = parseInt($(this).css('padding-bottom'));
+      
+      $(this).data('lev_padding-top', opts.padding_top);
+      $(this).data('lev_padding-bottom', opts.padding_bottom);
+      
+      // Give the element room to levhitate
+      $(this).animate({'padding-top': opts.padding_top+opts.range}, opts.speed*.8);
+      
+      
+      var element = this;
+      var interval = setInterval(function() {
+        applyLevitation(element, opts);  
+      }, opts.speed);
 
-    //Save interval so we can stop it later
-    this.data('lev_interval', interval);
-        
+      //Save interval so we can stop it later
+      $(this).data('lev_interval', interval);
+    });
+
     return this;
   };
   
